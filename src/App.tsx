a761d3cedd0d00a27dc4a4aeb5f8f1ff63504e38
Julia Tufts <juliatufts@gmail.com>
2025-10-14 14:43:01 -0400

    add form

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,49 @@ import ollie from "./assets/ollie.png";
 import "./App.css";
 
 function App() {
+  const defaultText = "hello printer";
+  const endpoint = "https://receipt.recurse.com/text";
+
+  const onSubmit = async (e: any) => {
+    e.preventDefault();
+    var formData = new FormData(e.target);
+    console.log(formData.get("text"));
+
+    try {
+      const response = await fetch(endpoint, {
+        method: "POST",
+        body: JSON.stringify({ text: formData.get("text") }),
+      });
+
+      if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+      }
+
+      const result = await response.json();
+      console.log(result);
+    } catch (error) {
+      let err = error as Error;
+      console.error(err.message);
+    }
+  };
+
   return (
     <>
       <h1>Vite Receipt</h1>
       <div className="card">
         <img src={ollie} className="logo" alt="Octopus logo" />
+      </div>
+      <div>
+        <form onSubmit={onSubmit}>
+          <textarea
+            id="text"
+            name="text"
+            rows={5}
+            cols={33}
+            defaultValue={defaultText}
+          ></textarea>
+          <button type="submit">Submit</button>
+        </form>
       </div>
     </>
   );
