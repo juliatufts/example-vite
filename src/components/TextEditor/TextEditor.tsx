e6efb6eb530e67ed864608956251b052cd1b0edc
Julia Tufts <juliatufts@gmail.com>
2025-11-04 18:41:42 -0500

    refactor back to textarea

--- src/components/TextEditor/TextEditor.tsx
+++ src/components/TextEditor/TextEditor.tsx
@@ -12,16 +12,12 @@ const initialStyles: TextStyles = {
 
 /**
  * Main editor component with textarea and options
- *
- * Unfortunately, in order to apply transform-scale to the textarea, we must use
- * a contenteditable div + an invisible input to handle the form data
  */
 function TextEditor({ id }: TextEditorProps) {
   const [textOptionStyles, setTextStyles] =
     React.useState<TextStyles>(initialStyles);
   const [containerStyles, setContainerStyles] = React.useState<TextStyles>({});
   const textAreaRef = useRef<HTMLDivElement>(null);
-  const textAreaInputRef = useRef<HTMLTextAreaElement>(null);
   const defaultText = "hello printer";
 
   useLayoutEffect(() => {
@@ -50,12 +46,6 @@ function TextEditor({ id }: TextEditorProps) {
     updateStyles(targetId, input, setTextStyles);
   }
 
-  function onTextInputChange(e: any) {
-    if (textAreaInputRef.current) {
-      textAreaInputRef.current.value = e.target?.textContent;
-    }
-  }
-
   return (
     <section onChange={onFormChange}>
       <div className="md:flex md:gap-2">
@@ -75,33 +65,9 @@ function TextEditor({ id }: TextEditorProps) {
                 "resize-none break-all border border-gray-300 rounded-md p-1",
                 textOptionStyles
               )}
-              onInput={onTextInputChange}
               defaultValue={defaultText}
             ></textarea>
           </div>
-          {/* <div className={cn("w-[420px] mb-4", containerStyles)}>
-            <div
-              role="textbox"
-              aria-labelledby={`${id}-text-label`}
-              contentEditable={true}
-              suppressContentEditableWarning={true}
-              className={cn(
-                "resize-none break-all border border-gray-300 rounded-md p-1",
-                textOptionStyles
-              )}
-              ref={textAreaRef}
-              onInput={onTextInputChange}
-            >
-              {defaultText}
-            </div>
-            <textarea
-              className="hidden"
-              id={`${id}-text`}
-              name={`${id}-text`}
-              defaultValue={defaultText}
-              ref={textAreaInputRef}
-            ></textarea>
-          </div> */}
         </div>
         <TextEditorOptions id={id} />
       </div>
